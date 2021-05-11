6cfc924fa164537b3c1e0c9f4e3891f2daa851e5
EpsilonOmega <martin.olivier1997@gmail.com>
2021-05-11 14:27:20 +0200

    converted makeplaylist to tinydb

--- makePlaylist.py
+++ makePlaylist.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 from scipy.spatial import distance
 import configparser
+import tinydb
 
 debugFlag = False 
 
@@ -20,7 +21,6 @@ def findClosest(currentTrack, data, alreadySeen, distFun):
             nextTrack = track
     return nextTrack 
 
-
 def loadData(dbName):
     with open(dbName) as jsonFile:
         data = json.load(jsonFile)
@@ -32,28 +32,35 @@ def main():
     config = configparser.ConfigParser()
     if debugFlag == True:
         config.read(r'configTest.cfg')
+        dbName = "test/" + config.get('Database', 'name')
     else:
         config.read(r'config.cfg')
+        dbName = config.get('Database', 'name') + '.json'
 
-    dbName = config.get('Database', 'name') + '.json'
     saveDir = config.get('Playlist', 'directory') 
     pName = config.get('Playlist', 'name')
     numTracks = int(config.get('Playlist', 'duration'))
     similarityFun = config.get('Playlist', 'similarityFun')
+    pMode = config.get('chiner', 'PartyMode')
+
+    if pMode == False:
+        playlistFile = open(pName + ".m3u", "w+")
     if similarityFun == "L2":
         distFun = distance.euclidean
     if similarityFun == "cosine":
         distFun = spatial.distance.cosine
 
-    data = loadData(dbName)
-    currentTrack = random.choice(data)
+    dbSong = tinydb.TinyDB(dbName)
+    
+    currentTrack = random.choice(dbSong.all())
     alreadySeen = []
-    with open(pName + ".m3u", "w+") as playlistFile:
-        for i in range(numTracks):
-            currentTrack = findClosest(currentTrack, data, alreadySeen, distFun)
-            alreadySeen.append(currentTrack)
+    for i in range(numTracks):
+        currentTrack = findClosest(currentTrack, dbSong.all(), alreadySeen, distFun)
+        alreadySeen.append(currentTrack)
+        if pMode == False:
             playlistFile.write(currentTrack['path'] + "\n")
-            #Empty already seen sometimes
+        #Empty already seen sometimes
+    #Party mode : stay active and add a track if mpd playlist < length
 
 if __name__ == "__main__":
     main()
