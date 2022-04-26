65a3e764a9bc489addad77ccda1c8f56666bc2e6
Martin Olivier <martin.olivier@linkfluence.com>
2022-04-26 17:53:25 +0200

    created simple main

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+import configparser
+import sqlite3
+
+from chiner import EmbedderAnalyzer
+
+def main():
+    config = configparser.ConfigParser()
+    config.read(r'config.cfg')
+    db_loc = os.path.join(config['database']['path'], config['database']['name'])
+    db_con = sqlite3.connect(db_loc)
+    db_cur = db_con.cursor()
+
+    embedder = EmbedderAnalyzer.from_config(config)
+
+    music_files = [os.path.join(path, name) for path, subdirs, files in \
+            os.walk(config['search_dir']['path']) for name in files]
+
+    if not os.path.isfile(db_loc):
+        db_cur.execute('''CREATE TABLE songs
+            (path text, embed text)''')
+
+    for i, song in enumerate(music_files):
+
+        embed = embedder.process(song)
+        
+        values  = "'" + os.path.abspath(song) + "'" 
+        values += "'" + embed + "'"
+        db_cur.execute("INSERT INTO songs" + \
+                "VALUES (" + values + ")") 
+
+
+if __name__ == "__main__":
+    main()
